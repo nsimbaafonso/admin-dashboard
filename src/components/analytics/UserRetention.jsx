8708562af18ef0ef2aa19769d757790e1e7daebe
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-07 23:57:53 +0100

    Atualização do título do gráfico de retençáo de usuários

--- src/components/analytics/UserRetention.jsx
+++ src/components/analytics/UserRetention.jsx
@@ -20,7 +20,7 @@ const UserRetention = () => {
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ delay: 0.5 }}
 		>
-			<h2 className='text-xl font-semibold text-gray-100 mb-4'>User Retention</h2>
+			<h2 className='text-xl font-semibold text-gray-100 mb-4'>Retenção de Usuários</h2>
 			<div style={{ width: "100%", height: 300 }}>
 				<ResponsiveContainer>
 					<LineChart data={userRetentionData}>
